70f2ead6a694ce02e2119b38fc07a6ef30818868
JJR3D <jrodriguez@qitec.net>
2023-01-23 18:09:36 +0100

    boards-lis changed to generate json

--- scripts/3dwork-boards-list.sh
+++ scripts/3dwork-boards-list.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Store the names of all folders in the current directory
-boards=$(ls /home/pi/printer_data/config/3dwork/boards/ -d */)
+# Store the names of all folders in the /boards directory
+folders=$(ls -d /home/pi/printer_data/config/3dwork/boards/*/)
 
-# Write the names to a text file
-echo "$boards" > boards_list.txt
+# Create a JSON object with the folder names
+json_data='{"boards":['
+for boards in $boards; do
+  json_data+='"'$boards'",'
+done
+json_data=${json_data%,}
+json_data+=']}'
 
-# Print the contents of the text file
-cat boards_list.txt
+# Write the JSON object to a file
+echo $json_data > boards_list.json

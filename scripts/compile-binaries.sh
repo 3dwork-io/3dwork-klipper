1c1bc0fe34feae1d400c0d49abd5a50bf0748803
JJR <jrodriugez@qitec.net>
2023-12-07 16:51:51 +0100

    Added new compile binaries boards

--- scripts/compile-binaries.sh
+++ scripts/compile-binaries.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+compile_btt-manta-e3ez() {
+    echo "Compiling firmware for BTT Manta E3 EZ"
+    cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-manta-e3ez/firmware.config /home/pi/klipper/.config
+    make olddefconfig
+    make clean
+    make
+    cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-manta-e3ez.bin
+}
+
 compile_btt-manta-m4p() {
     echo "Compiling firmware for BTT Manta M4P"
     cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-manta-m4p/firmware.config /home/pi/klipper/.config
@@ -7,6 +16,15 @@ compile_btt-manta-m4p() {
     make clean
     make
     cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-manta-m4p.bin
+}
+
+compile_btt-manta-m4p-22() {
+    echo "Compiling firmware for BTT Manta M4P v2.2"
+    cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-manta-m4p-22/firmware.config /home/pi/klipper/.config
+    make olddefconfig
+    make clean
+    make
+    cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-manta-m4p-22.bin
 }
 
 compile_btt-manta-m8p() {
@@ -18,7 +36,25 @@ compile_btt-manta-m8p() {
     cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-manta-m8p.bin
 }
 
-compile_octopus_pro_446() {
+compile_btt-manta-m8p-11() {
+    echo "Compiling firmware for BTT Manta M8P v1.1"
+    cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-manta-m8p-11/firmware.config /home/pi/klipper/.config
+    make olddefconfig
+    make clean
+    make
+    cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-manta-m8p-11.bin
+}
+
+compile_btt-octopus-max-ez() {
+    echo "Compiling firmware for BTT Octopus Max EZ"
+    cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-octopus-max-ez/firmware.config /home/pi/klipper/.config
+    make olddefconfig
+    make clean
+    make
+    cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-octopus-max-ez.bin
+}
+
+compile_btt-octopus-pro-446() {
     echo "Compiling firmware for BTT Octopus Pro 446"
     cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-octopus-pro-446/firmware.config /home/pi/klipper/.config
     make olddefconfig
@@ -27,7 +63,7 @@ compile_octopus_pro_446() {
     cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-octopus-pro-446.bin
 }
 
-compile_octopus_pro_429() {
+compile_btt-octopus-pro-429() {
     echo "Compiling firmware for BTT Octopus Pro 429"
     cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-octopus-pro-429/firmware.config /home/pi/klipper/.config
     make olddefconfig
@@ -36,7 +72,16 @@ compile_octopus_pro_429() {
     cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-octopus-pro-429.bin
 }
 
-compile_btt_octopus_11() {
+compile_btt-octopus-pro-h723() {
+    echo "Compiling firmware for BTT Octopus Pro H723"
+    cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-octopus-pro-h723/firmware.config /home/pi/klipper/.config
+    make olddefconfig
+    make clean
+    make
+    cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-octopus-pro-h723.bin
+}
+
+compile_btt-octopus-11() {
     echo "Compiling firmware for BTT Octopus v1.1"
     cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-octopus-11/firmware.config /home/pi/klipper/.config
     make olddefconfig
@@ -45,8 +90,8 @@ compile_btt_octopus_11() {
     cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-octopus-11.bin
 }
 
-compile_btt_octopus_11_407() {
-    echo "Compiling firmware for BTT Octopus v1.1"
+compile_btt-octopus-11-407() {
+    echo "Compiling firmware for BTT Octopus v1.1 (407)"
     cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-octopus-11-407/firmware.config /home/pi/klipper/.config
     make olddefconfig
     make clean
@@ -72,13 +117,22 @@ compile_skr_pro_12() {
     cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-skr-pro-12.bin
 }
 
-compile_skr_2_429() {
+compile_btt-skr-2-429() {
     echo "Compiling firmware for SKR 2 429"
     cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-skr-2-429/firmware.config /home/pi/klipper/.config
     make olddefconfig
     make clean
     make
     cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-skr-2-429.bin
+}
+
+compile_btt-skr-2-407() {
+    echo "Compiling firmware for SKR 2 407"
+    cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-skr-2-407/firmware.config /home/pi/klipper/.config
+    make olddefconfig
+    make clean
+    make
+    cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-skr-2-407.bin
 }
 
 compile_btt_ebb42_10() {
@@ -171,6 +225,42 @@ compile_btt_skr_3() {
     cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-skr-3.bin
 }
 
+compile_btt-skr-3-h723() {
+    echo "Compiling firmware for SKR 3 (H723)"
+    cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-skr-3-h723/firmware.config /home/pi/klipper/.config
+    make olddefconfig
+    make clean
+    make
+    cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-skr-3-h723.bin
+}
+
+compile_btt-skr-3-ez() {
+    echo "Compiling firmware for SKR 3 EZ"
+    cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-skr-3-ez/firmware.config /home/pi/klipper/.config
+    make olddefconfig
+    make clean
+    make
+    cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-skr-3-ez.bin
+}
+
+compile_btt-skr-3-ez-h723() {
+    echo "Compiling firmware for SKR 3 EZ (H723)"
+    cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-skr-3-ez-h723/firmware.config /home/pi/klipper/.config
+    make olddefconfig
+    make clean
+    make
+    cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-skr-3-ez-h723.bin
+}
+
+compile_btt-skrat-10() {
+    echo "Compiling firmware for SKR RAT v1.0"
+    cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-skrat-10/firmware.config /home/pi/klipper/.config
+    make olddefconfig
+    make clean
+    make
+    cp /home/pi/klipper/out/klipper.bin /home/pi/printer_data/config/firmware_binaries/firmware-btt-skrat-10.bin
+}
+
 compile_btt-skr-14-turbo() {
     echo "Compiling firmware for SKR 1.4 Turbo"
     cp -f /home/pi/printer_data/config/3dwork-klipper/boards/btt-skr-14-turbo/firmware.config /home/pi/klipper/.config
@@ -206,15 +296,30 @@ pushd /home/pi/klipper
 if [ -z "$1" ]; then
     # If no parameter is provided, compile firmware for all boards
     # Run make scripts for the supported boards.
+    compile_btt-manta-e3ez
     compile_btt-manta-m4p
+    compile_btt-manta-m4p-22
     compile_btt-manta-m8p
-    compile_octopus_pro_446
-    compile_octopus_pro_429
-    compile_btt_octopus_11
-    compile_btt_octopus_11_407
+    compile_btt-manta-m8p-11
+    compile_btt-octopus-max-ez
+    compile_btt-octopus-pro-446
+    compile_btt-octopus-pro-429
+    compile_btt-octopus-pro-h723
+    compile_btt-octopus-11
+    compile_btt-octopus-11-407
     compile_fysetc_spider
     compile_skr_pro_12
-    compile_skr_2_429
+    compile_btt_skr_mini_e3_30
+    compile_btt_skr_3
+    compile_btt-skr-3-h723
+    compile_btt-skr-3-ez
+    compile_btt-skr-3-ez-h723
+    compile_btt-skr-2-429
+    compile_btt-skr-2-407
+    compile_btt-skrat-10
+    compile_btt-skr-14-turbo
+    compile_znp_robin_nano_dw_v2
+    # Toolheads
     compile_btt_ebb42_10
     compile_btt_ebb36_10
     compile_btt_ebb42_11
@@ -223,10 +328,6 @@ if [ -z "$1" ]; then
     compile_btt_ebb36_12
     compile_mellow_fly_sht_42
     compile_mellow_fly_sht_36
-    compile_btt_skr_mini_e3_30
-    compile_btt_skr_3
-    compile_btt-skr-14-turbo
-    compile_znp_robin_nano_dw_v2
 else
     compile_function="compile_$1"
     if declare -f "$compile_function" >/dev/null; then
